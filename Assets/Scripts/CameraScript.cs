1e8ec7518df0f8235caa43abe0c9cf25fb071816
Daniel Verdejo <dverdagio@gmail.com>
2017-03-23 19:36:51 +0000

    First commit, camera script, spawner script, ui, & essentials added

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+
+    public GameObject cameraPlane;
+    public GameObject cameraParent = null;
+
+    // Use this for initialization
+    void Start () {
+
+        ////check to see if using a mobile platform
+        if (Application.isMobilePlatform)
+        {
+            // allows to look around in a natural way
+            cameraParent = new GameObject("cameraParent");
+            cameraParent.transform.position = this.transform.position; // set the parents position to the camera
+            this.transform.parent = cameraParent.transform;
+            cameraParent.transform.Rotate(Vector3.right, 90);
+        }//if
+
+        //if the device has a gyroscope in it use this will 
+        Input.gyro.enabled = true;
+
+
+        //create a new web cam texture and assign the camera plane its texture
+        //This takes the information from the camera and attaches that to the plabe
+        WebCamTexture camTexture = new WebCamTexture();
+        cameraPlane.GetComponent<MeshRenderer>().material.mainTexture = camTexture;
+        camTexture.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if using a mobile device use the gyro
+        if (Application.isMobilePlatform) {
+            //use quaternion to store information for view rotation by receiving data from the gyro
+            Quaternion rotationalFix = new Quaternion(Input.gyro.attitude.x, // left right
+                                                Input.gyro.attitude.y, //up down
+                                                -Input.gyro.attitude.z, // forward back
+                                                -Input.gyro.attitude.w); // rotational space in 360 degrees
+
+            //set the local rotation of the camera to the Quaternion value
+            this.transform.localRotation = rotationalFix;
+
+        }//if using a mobile device
+
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject [] enemy;
+    public float spawnDelay = 3f;
+    public Transform[] spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnDelay, spawnDelay);
+	}
+	
+    void Spawn(){
+        //if(health.current < 0f)
+        //{
+        //    //exit
+        //    return;
+        //}
+
+        int spawnIndex = Random.Range(0, spawnPoint.Length);
+        int enemyIndex = Random.Range(0, enemy.Length);
+
+        Instantiate(enemy[enemyIndex], spawnPoint[spawnIndex].position, spawnPoint[spawnIndex].rotation);
+    }
+}
